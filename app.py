d8e3a65bc4139ce9f35d10c55ad8a141ab7963da
Vinayaka Pullayyagari <pullayyagarivinayaka@gmail.com>
2026-09-16 12:05:57 +0530

    Remove unnecessary blank line in app.py

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ import sys
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 sys.path.insert(0, str(BASE_DIR))
 
 from resume_parser import extract_text, UnsupportedFileTypeError
